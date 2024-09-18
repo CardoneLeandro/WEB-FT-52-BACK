53688d9b556d6ae9c9cf27cabd3eb5bb51b7178c
CardoneLeandro <91804622+CardoneLeandro@users.noreply.github.com>
2024-09-18 19:17:41 -0300

    some modifies on product service

--- back/src/products/products.service.ts
+++ back/src/products/products.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { elementType } from 'src/common/enum/elementType.enum';
+import { ElementsRepository } from 'src/element/element.repository';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+constructor(private readonly elementRepo: ElementsRepository) {}
+
+  async create(Dto: CreateProductDto) {
+    const newProduct = {...Dto, type:elementType.PRODUCT}
+    const createdProduct = this.elementRepo.create(newProduct);
+    if(!createdProduct) {throw new Error('Could not create product');}
+    console.log('CARDONE => productsService, create, createdProduct', createdProduct);
+    const savedProduct = await this.elementRepo.save(createdProduct);
+    return savedProduct
+
   }
 
   findAll() {
     return `This action returns all products`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id:string) {
+    const existingProduct = this.elementRepo.findOneBy({id});
+    if(!existingProduct) {throw new Error('Could not find product');}
+    return existingProduct
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
