e11f64cc89969e5d27497590c5123cdea365baa0
Fernando Campellone <tomascampellone@gmail.com>
2024-09-24 18:57:22 -0300

    SSL CHANGE

--- back/config/typeorm.config.ts
+++ back/config/typeorm.config.ts
@@ -11,6 +11,7 @@ const config = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: false,
     logging: false,
     entities: ['dist/**/*.entity{.ts,.js}'],
     migrations: ['dist/migrations/*{.ts,.js}'],
