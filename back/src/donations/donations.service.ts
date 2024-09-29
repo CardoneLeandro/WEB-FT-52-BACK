0ce1bf681c10b7df86a3a15c98f39dee899c55bb
Fernando Campellone <tomascampellone@gmail.com>
2024-09-29 12:00:30 -0300

    Webhook v1.9

--- back/src/donations/donations.service.ts
+++ back/src/donations/donations.service.ts
@@ -13,21 +13,15 @@ export class DonationsService {
   }
 
   async webhook(xSignature: string, xRequestId: string, dataId: string){
-    // Obtain the x-signature value from the header
-    // const xSignature = Headers['x-signature']; // Assuming headers is an object containing request headers
-    // const xRequestId = Headers['x-request-id']; // Assuming headers is an object containing request headers
-
     // Obtain Query params related to the request URL
     // const urlParams = new URLSearchParams(window.location.search);
     // const dataID = urlParams.get('data.id');
-
     // Separating the x-signature into parts
     const parts = xSignature.split(',');
-
+    console.log('---------> PARTS == ',parts)
     // Initializing variables to store ts and hash
     let ts;
     let hash;
-
     // Iterate over the values to obtain ts and v1
     parts.forEach(part => {
         // Split each part into key and value
@@ -43,19 +37,21 @@ export class DonationsService {
         }
     });
 
+    console.log('--------------> TS == ', ts)
+    console.log('--------------> HASH == ', hash)
     // Obtain the secret key for the user/application from Mercadopago developers site
     const secret = process.env.WEBHOOK_SECRET_KEY;
-
+    console.log('-------------> SECRET == ', secret)
     // Generate the manifest string
     const manifest = `id:${dataId};request-id:${xRequestId};ts:${ts};`;
-
+    console.log('-------------> MANIFEST == ', manifest)
     // Create an HMAC signature
     const hmac = crypto.createHmac('sha256', secret);
     hmac.update(manifest);
-
+    console.log('---------------> HMAC == ',hmac)
     // Obtain the hash result as a hexadecimal string
     const sha = hmac.digest('hex');
-
+    console.log('---------------> SHA == ', sha)
     if (sha === hash) {
         // HMAC verification passed
         console.log("HMAC verification passed");
@@ -63,7 +59,7 @@ export class DonationsService {
         // HMAC verification failed
         console.log("HMAC verification failed");
     }
-    
+  
   }
 }
 
