7d43e2e465eac64dda18a0cbd91cda5176db17a8
Fernando Campellone <tomascampellone@gmail.com>
2024-10-05 14:31:52 -0300

    added an example on the ConfirmAssitanceRoute

--- back/src/events/dto/asistance-event.dts.ts
+++ back/src/events/dto/asistance-event.dts.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsUUID } from "class-validator";
+
 export class ConfirmAssistEventDto {
+    @ApiProperty({
+        description: 'ID del usuario que sube el evento',
+        example: 'a0a0a0a0-a0a0-a0a0-a0a0-a0a0a0a0a0a0',
+        type: String,
+      })
+    @IsUUID()
     creator: string
 }
