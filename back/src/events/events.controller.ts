5d8b53ef7a0e5eb0a32d3a26134eae44a5911627
Fernando Campellone <tomascampellone@gmail.com>
2024-10-05 15:06:49 -0300

    Deleted a comentary in eventControler

--- back/src/events/events.controller.ts
+++ back/src/events/events.controller.ts
@@ -109,7 +109,7 @@ export class EventsController {
     name: 'id',
     description: 'El ID del evento al que se quiere actualizar la asistencia',
     type: String,
-    example: '123e4567-e89b-12d3-a456-426614174000',  // Ejemplo de un UUID
+    example: '123e4567-e89b-12d3-a456-426614174000'
   })
   async updateAttendanceStatus(
     @Param('id', ParseUUIDPipe) id: string,
